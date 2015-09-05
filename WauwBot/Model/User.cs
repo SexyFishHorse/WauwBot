05f3ae8d14c8cdfa740575a40e1f6bea2e907f04
Asbjørn Hansen <asser-dk@outlook.com>
2015-09-05 14:15:04 +0200

    added user object

---
+++ WauwBot/Model/User.cs
@@ -0,0 +1,23 @@
+﻿namespace SexyFishHorse.WauwBot.Model
+{
+    using System;
+    using SexyFishHorse.WauwBot.DataAccess;
+
+    [StoredIn("WauwBot")]
+    public class User : MongoBase
+    {
+        public string Username { get; set; }
+
+        public string Alias { get; set; }
+
+        public Provider Provider { get; set; }
+
+        public DateTime FirstSeen { get; set; }
+
+        public DateTime LastSeen { get; set; }
+
+        public long AccumulatedMinutes { get; set; }
+
+        public bool Regular { get; set; }
+    }
+}
